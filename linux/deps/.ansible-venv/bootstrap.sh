0705aa3883a00fc6c69564d038db01834e4589a0
doomcraw <valeriy.z@devs.ae>
2025-08-06 18:21:29 +0400

    sync

--- linux/deps/.ansible-venv/bootstrap.sh
+++ linux/deps/.ansible-venv/bootstrap.sh
@@ -4,17 +4,18 @@ set -eu
 
 pushd $(dirname ${BASH_SOURCE})
 
+force=${1:-""}
 venv_name=".ansible-venv"
 venv_path=~/$venv_name
 pip_requirements="${PWD}/requirements.txt"
 galaxy_requirements="${PWD}/requirements.yml"
 
-if python3 -c 'import venv' > /dev/null 2>&1; then
+if ! python3 -c 'import venv' > /dev/null 2>&1; then
 	pip install pip --upgrade
 	pip install venv
 fi
 
-if [ "${1}" = '--force' -o "${1}" = '-f' ]; then
+if [ "${force}" = '--force' -o "${force}" = '-f' ]; then
 	rm -rf $venv_path
 fi
 
