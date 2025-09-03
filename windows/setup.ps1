c12fd7aceea292bdaf5112240b5bae92f2f13173
doomcraw <doomcraw@gmail.com>
2025-09-03 21:17:33 +0400

    sync

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -43,6 +43,8 @@ if ($Registry -or $All) {
 }
 
 # ############ ( ----------------     Services    ----------------) ############
+# Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\OneSyncSvc -Name Start -Value 4
+# Set-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\PhoneSvc -Name Start -Value 4
 
 
 if ($Registry -or $All) {
--- windows/wsl/setup.ps1
+++ windows/wsl/setup.ps1
@@ -1,7 +1,12 @@
-if ((& wsl.exe --status).length -eq 0) {
-    & wsl.exe --install --no-distribution
-    Set-Registry 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' @{InstallWSL = "$((Get-Command powershell.exe).Source) -NoLogo -ExecutionPolicy Bypass -File `"${PSCommandPath}`""}
-    Exit 0
+if (Get-Command wsl.exe -ErrorAction SilentlyContinue) {
+  if ((& wsl.exe --status).length -eq 0) {
+      & wsl.exe --install --no-distribution
+      Set-Registry 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' @{InstallWSL = "$((Get-Command powershell.exe).Source) -NoLogo -ExecutionPolicy Bypass -File `"${PSCommandPath}`""}
+      Exit 0
+  }
+} else {
+    Write-Host 'WSL not found on this system'
+    Exit 1
 }
 
 $wsl_dir='D:\vms\wsl'
@@ -20,11 +25,11 @@ if (!(Test-Path $wsl_dir)) {
 
 wsl --import $wsl_name $wsl_dir\$wsl_name $wsl_image.OutFile
 wsl --set-default $wsl_name
-wsl -d $wsl_name -u root --shell-type standard /bin/bash -c "/bin/bash `$(wslpath `"${PSScriptRoot}\firstboot.sh`") `"${Env:USERNAME.ToLower()}`""
+wsl -d $wsl_name -u root --shell-type standard /bin/bash -c "/bin/bash `$(wslpath `"${PSScriptRoot}\firstboot.sh`") `"$(${Env:USERNAME}.ToLower())`""
 wsl -t $wsl_name
 wsl -d $wsl_name -u root --shell-type standard /bin/bash -c "/bin/bash `$(wslpath `"${PSScriptRoot}\resolvconf.sh`")"
 wsl -d $wsl_name -u ${Env:USERNAME.ToLower()} --shell-type standard /bin/bash -c 'cd ~; curl -fsSL https://raw.githubusercontent.com/DoomCraw/dotfiles/refs/heads/main/linux/setup.sh | sudo /bin/bash -'
 
-Remove-Item $wsl_image.OutFile -Force
+Remove-Item -Path $wsl_image.OutFile -Force
 
 Exit 0
