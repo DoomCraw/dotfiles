a36321271d832f104a9fc2650846064b89090b71
doomcraw <doomcraw@gmail.com>
2026-07-29 19:08:20 +0400

    sync

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -27,57 +27,57 @@ set_ssh_perms () {
 sync_dotfiles
 set_ssh_perms
 
-pushd ~
+# pushd ~
 
-if [ ! -d .vim/autoload -a ! -d .vim/bundle ]; then
-	mkdir -p .vim/{autoload,bundle}
-	curl -fsSL https://tpo.pe/pathogen.vim -o .vim/autoload/pathogen.vim
+# if [ ! -d .vim/autoload -a ! -d .vim/bundle ]; then
+# 	mkdir -p .vim/{autoload,bundle}
+# 	curl -fsSL https://tpo.pe/pathogen.vim -o .vim/autoload/pathogen.vim
 
-	plugins=(
-    "https://github.com/vim-airline/vim-airline-themes"
-    "https://github.com/vim-airline/vim-airline"
-    "https://github.com/sheerun/vim-polyglot"
-    "https://github.com/sainnhe/everforest"
-    "https://github.com/morhetz/gruvbox"
-    "https://github.com/cocopon/iceberg.vim"
-    "https://github.com/nordtheme/vim.git nord"
-    "https://github.com/preservim/nerdtree"
-    "https://github.com/pearofducks/ansible-vim"
-    "https://github.com/neoclide/coc.nvim"
-    "https://github.com/martinda/Jenkinsfile-vim-syntax"
-    "https://github.com/dense-analysis/ale"
-	)
+# 	plugins=(
+#     "https://github.com/vim-airline/vim-airline-themes"
+#     "https://github.com/vim-airline/vim-airline"
+#     "https://github.com/sheerun/vim-polyglot"
+#     "https://github.com/sainnhe/everforest"
+#     "https://github.com/morhetz/gruvbox"
+#     "https://github.com/cocopon/iceberg.vim"
+#     "https://github.com/nordtheme/vim.git nord"
+#     "https://github.com/preservim/nerdtree"
+#     "https://github.com/pearofducks/ansible-vim"
+#     "https://github.com/neoclide/coc.nvim"
+#     "https://github.com/martinda/Jenkinsfile-vim-syntax"
+#     "https://github.com/dense-analysis/ale"
+# 	)
 
-	pushd .vim/bundle
+# 	pushd .vim/bundle
 
-	IFS=""
-	for plugin in "${plugins[@]}"; do
-	    git clone -q $plugin
-	done
+# 	IFS=""
+# 	for plugin in "${plugins[@]}"; do
+# 	    git clone -q $plugin
+# 	done
 
-	popd
+# 	popd
 
-	pushd .vim/bundle/coc.nvim
-	npm ci
-	popd
-fi
+# 	pushd .vim/bundle/coc.nvim
+# 	npm ci
+# 	popd
+# fi
 
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-	if [ ! -d ~/.tmux/plugins ]; then
-		mkdir -p ~/.tmux/plugins
-	fi
-	git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
+# if [ ! -d ~/.tmux/plugins/tpm ]; then
+# 	if [ ! -d ~/.tmux/plugins ]; then
+# 		mkdir -p ~/.tmux/plugins
+# 	fi
+# 	git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# fi
 
-if [ ! -f ~/.dircolors ]; then
-	dircolors --print-database > ~/.dircolors
-fi
+# if [ ! -f ~/.dircolors ]; then
+# 	dircolors --print-database > ~/.dircolors
+# fi
 
-git clone -q https://codeberg.org/Larvitz/nvim-ansible.git ~/.config/nvim
+# git clone -q https://codeberg.org/Larvitz/nvim-ansible.git ~/.config/nvim
 
-popd
+# popd
 
-popd
+# popd
 
 
 unset sync_dotfiles set_ssh_perms plugins plugin
